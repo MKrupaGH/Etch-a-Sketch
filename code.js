78ff796fa5e229d02a79b7784aa78061028c41a7
Marek <marekkrupa03@gmail.com>
2022-04-06 20:00:53 +0200

    Work end / project finished

--- code.js
+++ code.js
@@ -1,24 +1,45 @@
 //creating container with divs
 
 const container = document.querySelector(".container");
+const color = document.querySelector(".rgb");
+const reset = document.querySelector("button");
+const gridSizeInput = document.querySelector(".gridSize");
 
 let isDown = false;
+let size = 16;
+gridSizeInput.addEventListener("change", () => {
+  size = gridSizeInput.value;
+  container.innerHTML = "";
+  createDivs(size);
+});
 
-function createDivs() {
-  for (let i = 0; i < 16 * 16; i++) {
+function createDivs(gridSize) {
+  for (let i = 0; i < gridSize ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add(".cell");
     cell.style.border = "1px solid black";
     container.appendChild(cell);
+    console.log(gridSize);
+    container.setAttribute(
+      "style",
+      "grid-template: repeat(" +
+        gridSize +
+        ",auto)/repeat(" +
+        gridSize +
+        ",auto)"
+    );
 
     cell.addEventListener("mouseover", () => {
-      if (isDown===true) {
-        cell.style.backgroundColor = "red";
+      if (isDown === true) {
+        cell.style.backgroundColor = color.value;
       }
+    });
+
+    reset.addEventListener("click", () => {
+      cell.style.backgroundColor = "#fff";
     });
   }
 }
-createDivs();
 
 document.addEventListener("mousedown", () => {
   isDown = true;
@@ -27,6 +48,4 @@ document.addEventListener("mouseup", () => {
   isDown = false;
 });
 
-console.log(isDown);
-
-
+window.onload = createDivs(size);
